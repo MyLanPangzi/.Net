8842934abd1c6150077addb364566aef5c12038d
MyLanpangzi <1251723871@qq.com>
2019-01-22 21:54:00 +0800

    调整目录

---
+++ ASPNETCore/WebApplication2/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;}
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("AppSettings.json")
+                .Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseFileServer();
+            app.UseMvcWithDefaultRoute();
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    //throw new Exception("系统异常");
+            //    await context.Response.WriteAsync(this.Configuration["message"]);
+            //});
+        }
+    }
+}
