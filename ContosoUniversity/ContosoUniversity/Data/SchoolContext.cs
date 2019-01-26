258453629842d2a26eca1f2d9f8e6bf1a4283b65
MyLanpangzi <1251723871@qq.com>
2019-01-27 00:03:35 +0800

    添加SchoolContext

--- ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -1,9 +1,17 @@
 using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Data
 {
-    public class SchoolContext
+    public class SchoolContext : DbContext
     {
-     
+        public SchoolContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> enrollments { get; set; }
     }
 }
